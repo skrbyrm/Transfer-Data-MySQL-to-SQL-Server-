506ee4359dd62cf199d09705fd3463aeab28bf13
Şakir Bayram <90007713+skrbyrm@users.noreply.github.com>
2023-04-08 15:25:28 +0300

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,8 +1,9 @@
-import mysql.connector
+# Transfer data( include create tables, pk and fk's) from MySQL to SQL Server 08.04.2023 
 import pyodbc
 import datetime
 from sqlalchemy import create_engine
 import pandas as pd
+import pymysql
 
 # MySQL connection env. 
 db_user = <some>
@@ -12,8 +13,8 @@ db_port = <some>
 db_name = <some>
 
 # Connect to MySQL database
-cnx_mysql = mysql.connector.connect(user=db_user, password=db_password,
-                                     host=db_host, port=db_port, database=db_name)
+cnx_mysql = pymysql.connect(user=db_user, password=db_password,
+                             host=db_host, port=int(db_port), db=db_name)
 
 # Connect to SQL Server database
 cnx_sql = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
@@ -24,7 +25,6 @@ cnx_sql = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
 
 # Set up SQLAlchemy engine for SQL Server connection
 engine_sql = create_engine('mssql+pyodbc://sa:<some>@<some>/<some>?driver=ODBC+Driver+17+for+SQL+Server')
-
 # Get all table names from MySQL database
 cursor_mysql = cnx_mysql.cursor()
 cursor_mysql.execute("SHOW TABLES")
@@ -38,15 +38,30 @@ for table in tables:
     cursor_sql = cnx_sql.cursor()
     cursor_sql.execute(f"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{table_name}'")
     table_exists = len(cursor_sql.fetchall()) > 0
-
+    
     if not table_exists:
         # Fetch column names and data types from MySQL table
         cursor_mysql.execute(f"DESCRIBE {table_name}")
         columns = cursor_mysql.fetchall()
         columns_sql = [f"{col[0].replace('İ', 'I')} {'float' if 'double' in col[1] else col[1].replace('tinyint(1)', 'tinyint')}" for col in columns]
 
+        cursor_mysql.execute(f"SHOW INDEX FROM {table_name}")
+        indexes = cursor_mysql.fetchall()
+        pk_cols = [idx[4] for idx in indexes if idx[2] == 'PRIMARY']
+        if len(pk_cols) > 0:
+            pk_constraint = f", CONSTRAINT PK_{table_name} PRIMARY KEY ({','.join(pk_cols)})"
+        else:
+            pk_constraint = ''
+        index_cols = [idx[4] for idx in indexes if idx[2] != 'PRIMARY']
+        if len(index_cols) > 0:
+            index_constraints = [f"INDEX IX_{table_name}_{col} ({col})" for col in index_cols]
+            index_constraint = f", {','.join(index_constraints)}"
+        else:
+            index_constraint = ''
+        
+        
         # Generate SQL statement to create table in SQL Server
-        query = f"CREATE TABLE {table_name} ({','.join(columns_sql)})"
+        query = f"CREATE TABLE {table_name} ({','.join(columns_sql)}{pk_constraint}{index_constraint})"
         print(query) # print the generated query
 
         # Execute query to create table in SQL Server database
@@ -63,6 +78,7 @@ for table in tables:
     chunk_size = 10000 # number of rows to read at a time
     offset = 0
     while True:
+        start_time = datetime.datetime.now()
         query = f"SELECT * FROM {table_name} WHERE createdAt > '{latest_timestamp}' LIMIT {chunk_size} OFFSET {offset}"
         data = pd.read_sql(query, cnx_mysql)
 
@@ -73,7 +89,11 @@ for table in tables:
         # Write data to SQL Server table
         try:
             data.to_sql(name=table_name, con=engine_sql, if_exists='append', index=False)
-            print(f"Data transfer for table {table_name} ; {len(data)} rows was successful.")
+            end_time = datetime.datetime.now()
+            elapsed_time = end_time - start_time
+            elapsed_seconds = int(elapsed_time.total_seconds())
+            minutes, seconds = divmod(elapsed_seconds, 60)
+            print(f"Data transfer for table {table_name} ; {len(data)} rows was successful in: {minutes} minutes {seconds} seconds.")
         except Exception as e:
             print(f"Data transfer for table {table_name} failed with error: {str(e)}")
 
